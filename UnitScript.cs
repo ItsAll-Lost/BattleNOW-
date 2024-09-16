e0473739c42fa130dee1e2a11983e4c929d4c9c6
ItsAll-Lost <143014059+ItsAll-Lost@users.noreply.github.com>
2024-09-16 15:35:35 -0500

    Update UnitScript.cs

--- UnitScript.cs
+++ UnitScript.cs
@@ -1,1 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitScript : MonoBehaviour
+{
+    public string unitName;
+    public int unitLevel;
+
+    public int damage;
+    public int speed;
+
+
+    public int maxHp;
+    public int currentHp;
+
+    public bool TakeDamage(int dmg)
+    {
+        currentHp -= dmg;
+
+        if (currentHp <= 0)
+            return true;
+        else
+            return false;
+    }
+
+    public void Heal(int amount)
+    {
+        currentHp += amount;
+        if (currentHp > maxHp)
+            currentHp = maxHp;
+    }
+
+}
 

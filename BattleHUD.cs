8ee7035e8aba16fb4a40294f23c6b5d994bcec8e
ItsAll-Lost <143014059+ItsAll-Lost@users.noreply.github.com>
2024-09-16 15:34:50 -0500

    Update BattleHUD.cs

--- BattleHUD.cs
+++ BattleHUD.cs
@@ -1,1 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
+
+public class BattleHUD : MonoBehaviour
+{
+    public TMP_Text nameText;
+    public TMP_Text levelText;
+    public Slider hpSlider;
+
+    public void SetHUD(UnitScript unit)
+    {
+        nameText.text = unit.unitName;
+        levelText.text = "" + unit.unitLevel;
+        hpSlider.maxValue = unit.maxHp;
+        hpSlider.value = unit.currentHp;
+    }
+
+    public void SetHp(int hp)
+    {
+        hpSlider.value = hp;
+    }
+}
